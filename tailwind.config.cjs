7fdc1ebc57b4460fb0dac2c5259abf10b735712a
bassamfouad <b.foaud@cyclus.digital>
2023-03-24 16:13:09 +0200

    adjusted tailwind.config settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{html,js,jsx,svelte,ts,tsx}'],
 	theme: {
 		extend: {},
 		fontFamily: {
