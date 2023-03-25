6636693ff06bf1280a042b1d249e0bc68ff01002
bassamfouad <b.foaud@cyclus.digital>
2023-03-25 20:47:40 +0200

    adjusted vercel config for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		})
 	},
 	preprocess: vitePreprocess()
 };
