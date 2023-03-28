73bc15a165a3fca97888410d4a0a69d6a598a9c0
bassamfouad <b.foaud@cyclus.digital>
2023-03-28 14:10:28 +0200

    added sitemap.xml

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x'
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
